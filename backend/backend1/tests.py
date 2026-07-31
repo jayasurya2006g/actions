a3f8e70683f5b1bb112de6b775dafae9ecf5090f
Jaya Surya <jayasurya2006g@gmail.com>
2026-07-31 19:56:26 +0530

    with test case one

--- backend/backend1/tests.py
+++ backend/backend1/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HelloTest(TestCase):
+
+    def test_hello_url(self):
+        response = self.client.get(reverse("hello"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hello")
